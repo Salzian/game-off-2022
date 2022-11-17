b6ee6f76b2fb5564aa17b7196bd84e7efdfbdc8c
Salzian <salzian.dev@gmail.com>
2022-11-17 23:11:42 +0100

    Refactored the paddle into a Player controller plugin with basic camera movement

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,13 @@
-use bevy::prelude::*;
+use bevy::prelude::App;
+use bevy::DefaultPlugins;
+use game_plugin::GamePlugin;
+
+mod game_plugin;
+mod player;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            window: WindowDescriptor {
-                fit_canvas_to_parent: true,
-                ..default()
-            },
-            ..default()
-        }))
-        .add_startup_system(setup)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(GamePlugin)
         .run();
-}
-
-// See https://bevyengine.org/examples/3d/3d-scene/
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
-
-    // cube
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
-
-    // light
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    });
-
-    // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
 }
