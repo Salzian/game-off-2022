88010aead8e75d9f6b5f1b38a886add52ff4d813
Abdelrahman Abdelhafez <hi@abdelrahman.sh>
2022-11-19 18:40:59 +0100

    Remove commented code

--- src/game_plugin.rs
+++ src/game_plugin.rs
@@ -10,7 +10,6 @@ impl Plugin for GamePlugin {
         app.add_plugin(PlayerPlugin)
             .add_plugin(PathmeshPlugin)
             .add_startup_system_to_stage(StartupStage::PreStartup, load_assets)
-            //.add_startup_system(setup_scene)
             .add_startup_system(setup_camera)
             .add_system(await_loading_assets)
             .add_system(move_camera);
@@ -32,48 +31,6 @@ fn load_assets(asset_server: Res<AssetServer>, mut commands: Commands) {
     commands.insert_resource(PathMeshResource {
         path_mesh: handle,
         loaded: false,
-    });
-}
-
-// TODO Evaluate scene system
-fn setup_scene(
-    mut commands: Commands,
-    mut mesh_assets: ResMut<Assets<Mesh>>,
-    path_mesh_assets: Res<Assets<PathMesh>>,
-    mut material_assets: ResMut<Assets<ColorMaterial>>,
-    asset_server: Res<AssetServer>,
-    windows: Res<Windows>,
-    // path_mesh_resources: Res<PathMeshResource>,
-) {
-    // commands.insert_resource(ClearColor(Color::BLACK));
-
-    // commands.insert_resource(PathMeshResource(path_mesh_handle));
-    // let duck_texture = asset_server.load("textures/duck.png");
-
-    // commands.spawn(SpriteBundle {
-    //     texture: duck_texture,
-    //     ..default()
-    // });
-
-    // let handle = &path_mesh_resources.path_mesh;
-
-    let handle = asset_server.load("arena.polyanya.mesh");
-    let arena_mesh = path_mesh_assets.get(&handle).unwrap();
-
-    let mesh_size = Vec2::new(49.0, 49.0);
-    let window = windows.primary();
-    let factor = (window.width() / mesh_size.x).min(window.height() / mesh_size.y);
-
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: mesh_assets.add(arena_mesh.to_mesh()).into(),
-        material: material_assets.add(ColorMaterial::from(Color::GRAY)),
-        transform: Transform::from_translation(Vec3::new(
-            -mesh_size.x / 2.0 * factor,
-            -mesh_size.y / 2.0 * factor,
-            0.0,
-        ))
-        .with_scale(Vec3::splat(factor)),
-        ..default()
     });
 }
 
