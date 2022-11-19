f32eb4d09a8f7e6cae3400524543451d41900272
Abdelrahman Abdelhafez <hi@abdelrahman.sh>
2022-11-19 19:43:12 +0100

    Clean up arena spawning logic

--- src/game_plugin.rs
+++ src/game_plugin.rs
@@ -11,63 +11,78 @@ impl Plugin for GamePlugin {
             .add_plugin(PathmeshPlugin)
             .add_startup_system(load_assets)
             .add_startup_system(setup_camera)
-            .add_system(await_loading_assets)
+            .add_system(setup_arena)
             .add_system(move_camera);
     }
+}
+
+#[derive(Resource, Default)]
+struct PathMeshResource {
+    handle: Handle<PathMesh>,
+    size: Vec2,
+    loaded: bool,
+}
+
+fn load_assets(asset_server: Res<AssetServer>, mut commands: Commands) {
+    let arena_path_mesh_handle = asset_server.load("arena.polyanya.mesh"); // TODO: Move into meshes directory
+
+    commands.insert_resource(PathMeshResource {
+        handle: arena_path_mesh_handle,
+        size: Vec2::new(49.0, 49.0),
+        ..default()
+    });
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-#[derive(Resource)]
-struct PathMeshResource {
-    path_mesh: Handle<PathMesh>,
-    loaded: bool,
-}
-
-fn load_assets(asset_server: Res<AssetServer>, mut commands: Commands) {
-    let handle = asset_server.load("arena.polyanya.mesh");
-    commands.insert_resource(PathMeshResource {
-        path_mesh: handle,
-        loaded: false,
-    });
-}
-
-fn await_loading_assets(
+fn setup_arena(
+    windows: Res<Windows>,
     path_mesh_assets: Res<Assets<PathMesh>>,
-    mut path_mesh_resource: ResMut<PathMeshResource>,
     mut commands: Commands,
+    mut path_mesh_resource: ResMut<PathMeshResource>,
     mut mesh_assets: ResMut<Assets<Mesh>>,
     mut material_assets: ResMut<Assets<ColorMaterial>>,
-    windows: Res<Windows>,
 ) {
-    let arena_mesh = path_mesh_assets.get(&path_mesh_resource.path_mesh);
+    let potential_arena_path_mesh = path_mesh_assets.get(&path_mesh_resource.handle);
 
-    match arena_mesh {
+    match potential_arena_path_mesh {
+        // If the resource hasn't been loaded yet:
         None => {
             return;
         }
+        // If the resource has been loaded before:
         Some(_) => {
-            path_mesh_resource.loaded = true;
+            // Was the resource already marked as loaded?
+            if path_mesh_resource.loaded {
+                return;
+            }
+            // Has the resource just been freshly loaded?
+            else {
+                path_mesh_resource.loaded = true; // Mark it as loaded
+            }
         }
     }
 
-    let arena_mesh = arena_mesh.unwrap();
-
-    let mesh_size = Vec2::new(49.0, 49.0);
     let window = windows.primary();
-    let factor = (window.width() / mesh_size.x).min(window.height() / mesh_size.y);
+
+    let arena_path_mesh = potential_arena_path_mesh.unwrap();
+    let arena_mesh = arena_path_mesh.to_mesh();
+
+    let arena_mesh_scaling_factor = (window.width() / path_mesh_resource.size.x)
+        .min(window.height() / path_mesh_resource.size.y);
+    let arena_mesh_translation = Vec3::new(
+        -path_mesh_resource.size.x / 2.0 * arena_mesh_scaling_factor,
+        -path_mesh_resource.size.y / 2.0 * arena_mesh_scaling_factor,
+        0.0,
+    );
 
     commands.spawn(MaterialMesh2dBundle {
-        mesh: mesh_assets.add(arena_mesh.to_mesh()).into(),
+        mesh: mesh_assets.add(arena_mesh).into(),
         material: material_assets.add(ColorMaterial::from(Color::GRAY)),
-        transform: Transform::from_translation(Vec3::new(
-            -mesh_size.x / 2.0 * factor,
-            -mesh_size.y / 2.0 * factor,
-            0.0,
-        ))
-        .with_scale(Vec3::splat(factor)),
+        transform: Transform::from_translation(arena_mesh_translation)
+            .with_scale(Vec3::splat(arena_mesh_scaling_factor)),
         ..default()
     });
 }
