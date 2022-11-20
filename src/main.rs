e5d540c3bb53dee497e8040dbb7d032e2cde2999
Abdelrahman Abdelhafez <hi@abdelrahman.sh>
2022-11-20 19:56:41 -0100

    Spawn arena (#10)
    * Rename player.rs to player_plugin.rs
    
    * Rename variables
    
    * Install bevy_pathmesh
    
    * Install plyanya
    
    * Fix window width and height
    
    * Test
    
    * Load mesh in a very ugly way 🤮
    
    * Rename arena-merged.polyanya.mesh to arena.polyanya.mesh
    
    * Remove commented code
    
    * Abstain from using stages
    
    * Clean up arena spawning logic
    
    * Remove fixed window sizes
    
    * Spawn player on top
    
    * Move meshes into dedicated directory
    
    Co-authored-by: Salzian <salzian.dev@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use bevy::prelude::App;
-use bevy::DefaultPlugins;
+use bevy::prelude::*;
 use game_plugin::GamePlugin;
 
 mod game_plugin;
-mod player;
+mod player_plugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                title: "Game Off 2022".to_string(),
+                fit_canvas_to_parent: true,
+                ..default()
+            },
+            ..default()
+        }))
         .add_plugin(GamePlugin)
         .run();
 }
